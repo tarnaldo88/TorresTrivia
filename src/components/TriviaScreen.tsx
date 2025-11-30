5e57824b7755d85edd550e3f15569e8bc74ae6ca
tarnaldo88 <tarnaldo88@gmail.com>
2025-11-29 22:58:56 -0800

    starting to implement trivia screen

--- src/components/TriviaScreen.tsx
+++ src/components/TriviaScreen.tsx
@@ -18,11 +18,30 @@ export const TriviaScreen: React.FC<TriviaScreenProps> = ({
     roundDuration = 120,
     onRoundEnd,
 }) => {
+    const [showAnswer,setShowAnswer] = useState(false);
+
+
+    const toggleAnswer = () => {
+        setShowAnswer(!showAnswer);
+    };
 
     return(
         <ImageBackground source={require('../assets/torresTrivia.png')}>
             <View>
-                
+                <View>
+                    <Text>Question:</Text>
+                </View>
+                <View>
+                    <TouchableOpacity>                        
+                        <Text>Show Answer</Text>
+                    </TouchableOpacity>
+                    <View>
+                        (showAnswer) ? 
+                            <Text> Answer</Text> 
+                        :
+                            
+                    </View>
+                </View>
             </View>
         </ImageBackground>
 
