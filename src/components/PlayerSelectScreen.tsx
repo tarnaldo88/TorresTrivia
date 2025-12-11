58dafda071c1f5f7d491e9b35f6b064bc9acd56b
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-11 11:20:21 -0800

    adding audio imports for player select

--- src/components/PlayerSelectScreen.tsx
+++ src/components/PlayerSelectScreen.tsx
@@ -1,4 +1,5 @@
 import {useState} from 'react';  
+import { Audio } from 'expo-av';
 import React from 'react';
 import { 
     View, 
@@ -9,6 +10,11 @@ import {
     ImageBackground, 
 } from 'react-native';
 import { useNavigation, useFocusEffect } from '@react-navigation/native';
+
+import thisMegan from '../assets/audio/this/thisisMegan.mp3';
+import thisEmilio from '../assets/audio/this/thisisEmilio.mp3';
+import thisKai from '../assets/audio/this/thisisKai.mp3';
+import thisAmaya from '../assets/audio/this/thisisAmaya.mp3';
 
 export const PlayerSelectScreen: React.FC = () =>  {
     const [playerList, setPlayerList] = useState<string[]>([]);
