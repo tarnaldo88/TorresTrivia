c66b08e9c21a1ea159cf7f9db58deaf74c831d86
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-11 10:58:11 -0800

    adding player list and method to add to list

--- src/components/PlayerSelectScreen.tsx
+++ src/components/PlayerSelectScreen.tsx
@@ -11,6 +11,11 @@ import {
 import { useNavigation, useFocusEffect } from '@react-navigation/native';
 
 export const PlayerSelectScreen: React.FC = () =>  {
+    const [playerList, setPlayerList] = useState<string[]>([]);
+
+    const addtoPlayerList = (player:string) => {
+        setPlayerList(prev => [...prev, player]);
+    };
 
     return(
         <View>
