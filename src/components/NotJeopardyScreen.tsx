a9a650a412edfab94dac56cc34e279203de89c93
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-23 15:43:50 -0400

    starting to implement not jeopardy

--- src/components/NotJeopardyScreen.tsx
+++ src/components/NotJeopardyScreen.tsx
@@ -18,6 +18,19 @@ export const NotJeopardyScreen: React.FC<NotJeopardyScreenProps> = ({
     roundDuration = 120,
     onRoundEnd,
 }) => {
+    const [pl1, setpl1] = useState(0);
+    const [pl2, setpl2] = useState(0);
+    const [pl3, setpl3] = useState(0);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          onRoundEnd && onRoundEnd(pl1 + pl2 + pl3);
+        }, roundDuration * 1000);
+    
+        return () => clearTimeout(timer);
+      }, [pl1, pl2, pl3, onRoundEnd, roundDuration]);
+
+    
 
     return(
         <ImageBackground source={require('../assets/torresTrivia.png')}>
