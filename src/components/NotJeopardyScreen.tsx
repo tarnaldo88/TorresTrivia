10543d969b74633a2a6390c9436978b4003b5946
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-23 17:39:58 -0400

    creation of stylesheet

--- src/components/NotJeopardyScreen.tsx
+++ src/components/NotJeopardyScreen.tsx
@@ -63,3 +63,11 @@ export const NotJeopardyScreen: React.FC<NotJeopardyScreenProps> = ({
 
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
