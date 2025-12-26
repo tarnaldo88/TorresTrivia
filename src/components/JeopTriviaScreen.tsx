cc00b72105b29e93c52ca8bed3bb3f338cdfcf04
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-26 15:19:33 -0400

    starting to implement trivia done in jeopardy style

---
+++ src/components/JeopTriviaScreen.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';  
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    TouchableOpacity, 
+    ImageBackground,
+    ActivityIndicator,
+} from 'react-native';
+import { TriviaDatabase } from '../services/triviaDatabase';
+import { TriviaQuestion } from '../types/index';
+
+interface JeopTriviaScreenProps {
+    roundDuration?: number;
+    onRoundENd?: (finalScore: number) => void;
+}
