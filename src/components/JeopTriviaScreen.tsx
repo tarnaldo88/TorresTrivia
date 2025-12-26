0e5ed974d3f40cac888e369524a21588fd5015b4
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-26 15:22:44 -0400

    starting to make screen

--- src/components/JeopTriviaScreen.tsx
+++ src/components/JeopTriviaScreen.tsx
@@ -14,3 +14,18 @@ interface JeopTriviaScreenProps {
     roundDuration?: number;
     onRoundENd?: (finalScore: number) => void;
 }
+
+export const JeopTriviaScreen: React.FC<JeopTriviaScreenProps> = () => {
+    const [showAnswer, setShowAnswer] = useState(false);
+    const [currentQuestion, setCurrentQuestion] = useState<TriviaQuestion | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [triviaDb, setTriviaDb] = useState<TriviaDatabase | null>(null);
+    const [questionsAnswered, setQuestionsAnswered] = useState(0);
+
+
+    return(
+        <View>
+            <Text>in progress</Text>
+        </View>
+    );
+}
