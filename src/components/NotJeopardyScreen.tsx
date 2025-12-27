03ef52779a88b47c737e2871a0cc0ee2ed596eca
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-27 14:49:47 -0400

    removed unnecessary type prop

--- src/components/NotJeopardyScreen.tsx
+++ src/components/NotJeopardyScreen.tsx
@@ -16,13 +16,11 @@ interface NotJeopardyScreenProps {
     onRoundEnd?: (finalScore: number) => void;
 }
 
-type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
-
 export const NotJeopardyScreen: React.FC<NotJeopardyScreenProps> = ({
     roundDuration = 120,
     onRoundEnd,
 }) => {
-    const navigation = useNavigation<HomeScreenNavigationProp>();
+    const navigation = useNavigation();
     const [pl1, setpl1] = useState(0);
     const [pl2, setpl2] = useState(0);
     const [pl3, setpl3] = useState(0);
