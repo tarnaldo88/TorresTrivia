7f1a2f8a0065ccef253eacb061c11e1805411e63
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-29 11:43:01 -0400

    beginning to add screen to make custom trivia questions

---
+++ src/components/AddTriviaQuestionScreen.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';  
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    TouchableOpacity, 
+    ImageBackground,
+    ActivityIndicator,
+} from 'react-native';
+import { TriviaDatabase } from '../services/triviaDatabase';
+import { TriviaQuestion } from '../types/index';
