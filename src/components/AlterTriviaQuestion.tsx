2892bfb2bfcd289cfb9965b0e55c12b49a10d879
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-01 12:20:47 -0400

    adding hooks necessary for storing user input to alter trivia question

--- src/components/AlterTriviaQuestion.tsx
+++ src/components/AlterTriviaQuestion.tsx
@@ -13,6 +13,11 @@ import { TriviaDatabase } from '../services/triviaDatabase';
 
 export const AlterTriviaQuestionScreen= React.FC = () => {
     const [triviaDb, setTriviaDb] = useState<TriviaDatabase | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [difficulty, setDifficulty] = useState('');
+    const [question, setQuestion] = useState('');
+    const [answer, setAnswer] = useState('');
+    const [category, setCategory] = useState('');
 
 
     return(
