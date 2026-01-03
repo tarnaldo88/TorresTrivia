cfbff54038c1642f970580ac8126f9ea72f4d1c0
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-03 08:39:38 -0400

    implemented placeholder and cur quest hook

--- src/components/QuestionList.tsx
+++ src/components/QuestionList.tsx
@@ -1,2 +1,13 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, FlatList } from 'react-native';
+import { TriviaQuestion } from '../types/index';
+
+const QuestionList: React.FC = () => {
+    const [currentQuestion, setCurrentQuestion] = useState<TriviaQuestion | null>(null);
+
+    return (
+        <View>
+            <Text>Question List</Text>
+        </View>
+    );
+}
