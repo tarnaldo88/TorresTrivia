387b3d198dbeab6677696a2bdcbadbaf74abe318
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-03 08:40:59 -0400

    created handle next, for going to next question

--- src/components/QuestionList.tsx
+++ src/components/QuestionList.tsx
@@ -5,6 +5,12 @@ import { TriviaQuestion } from '../types/index';
 const QuestionList: React.FC = () => {
     const [currentQuestion, setCurrentQuestion] = useState<TriviaQuestion | null>(null);
 
+    const handleNext = () => {
+        const currentIndex = QuestionList.findIndex((q) => q === currentQuestion);
+        const nextIndex = (currentIndex + 1) % QuestionList.length;
+        setCurrentQuestion(QuestionList[nextIndex]);
+    }
+
     return (
         <View>
             <Text>Question List</Text>
