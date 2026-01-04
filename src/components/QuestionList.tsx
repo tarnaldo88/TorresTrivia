3c08b16efc09eae4aa1f1b594ff69f1ff87a54c2
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-04 14:21:34 -0400

    starting to make ui that will display questions

--- src/components/QuestionList.tsx
+++ src/components/QuestionList.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, FlatList } from 'react-native';
 import { TriviaQuestion } from '../types/index';
 
-export const QuestionList: React.FC = () => {
+export const QuestionList: React.FC = ({questions}) => {
     // const [currentQuestion, setCurrentQuestion] = useState<TriviaQuestion | null>(null);
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
@@ -23,28 +23,10 @@ export const QuestionList: React.FC = () => {
     return (
         <View>
             <Text>Question List</Text>
-            <FlatList
-                data={QuestionList}
-                keyExtractor={(question) => question.id.toString()}
-                renderItem={({ item }) => (
-                    <TouchableOpacity onPress={() => setCurrentQuestion(item)}>
-                        <Text>Question: {item.id}</Text>
-                        <Text>{item.question}</Text>
-                        <Text>Answer: {item.answer}</Text>
-                    </TouchableOpacity>
-                )}
-            />
-            {currentQuestion && (
-                <View>
-                    <TouchableOpacity onPress={handleNext}>
-                        <Text>Next</Text>
-                    </TouchableOpacity>
-                    <Text>Current Question: {currentQuestion?.question}</Text>
-                    <TouchableOpacity onPress={() => setCurrentQuestion(null)}>
-                        <Text>Clear</Text>
-                    </TouchableOpacity>
-                </View>
-            )}
+            {questions.length > 0 ? (
+                
+            )
+            }
             
         </View>
     );
